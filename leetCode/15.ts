9bb540e493c5d4ddac3e376de360d8cc7c78c9a8
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-11-10 14:22:02 +0300

    Create 15.ts

---
+++ leetCode/15.ts
@@ -0,0 +1,11 @@
+function threeSum(nums: number[]): number[][] {
+
+ const  answer:number[][] = []
+
+  let min =  Math.min.apply(Math,nums);
+  let max = Math.max.apply(Math,nums);
+  
+ 
+
+return answer;
+};
